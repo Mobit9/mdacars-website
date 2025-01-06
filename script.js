aa15519fc699b800c282fe8ddab2559772f611cc
Mobit9 <07dragos@gmail.com>
2025-01-06 07:55:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-// Configurare Mapbox
-mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [26.1, 44.4],
-    zoom: 10
-});
-
-// Marcaje pe hartă
-const prestatori = [
-    { lng: 26.1025, lat: 44.4268, nume: 'Vulcanizare Non-Stop', tip: 'vulcanizare' },
-    { lng: 26.085, lat: 44.435, nume: 'Tractare Rapidă București', tip: 'tractare' }
-];
-
-prestatori.forEach(prestator => {
-    new mapboxgl.Marker()
-        .setLngLat([prestator.lng, prestator.lat])
-        .setPopup(new mapboxgl.Popup().setHTML(`<h3>${prestator.nume}</h3><p>${prestator.tip}</p>`))
-        .addTo(map);
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Mesajul tău a fost trimis! Mulțumim pentru feedback.");
 });
